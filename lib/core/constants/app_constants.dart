d508fa5e0f7d2aeb8afc3e857015713012fe696d
Usman <usmanbinkhalidpk@gmail.com>
2026-03-15 10:13:55 +0500

    Image and Fonts Directories Completed

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,4 +1,8 @@
 class AppConstants {
   static const String appName = 'Kuponna';
   static const String apiBaseUrl = 'https://api.example.com';
+
+  // Assets
+  static const String splashLogo = 'assets/images/splash.png';
+  static const String onboardingImage = 'assets/images/Frame.png';
 }
