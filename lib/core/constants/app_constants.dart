bcb16bfd70e124c4ed01e69121aa0bd283f631c4
Usman <usmanbinkhalidpk@gmail.com>
2026-03-21 15:51:01 +0500

    Images Store

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -10,4 +10,12 @@ class AppConstants {
   static const String merchantIcon = 'assets/images/merchant.png';
   static const String googleIcon = 'assets/images/google_icon.png';
   static const String verifyCode = 'assets/images/verify_code.png';
+  static const String banana = 'assets/images/banana.png';
+  static const String roll = 'assets/images/roll.png';
+  static const String carrat = 'assets/images/carrat.png';
+  static const String suchi = 'assets/images/suchi.jpg';
+  static const String pant = 'assets/images/pant.jpg';
+  static const String rose = 'assets/images/red_rose.jpg';
+  static const String profile = 'assets/images/profile.png';
+  static const String front = 'assets/images/front.png';
 }
